87726c98cffeebf1e385d220f239fbb0c806d22a
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-08-12 11:13:04 -0700

    seeds info for cars api

--- seeds/01-users.js
+++ seeds/01-users.js
@@ -1,13 +1,14 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('users').truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('users').insert([
+        { VIN: "10293495", make: "Acura", model: "Compact", mileage: "192000" },
+        { VIN: "23450223", make: "BMW", model: "Midsize", mileage: "102000" },
+        { VIN: "34459009", make: "Honda", model: "Minivan", mileage: "450690" },
+        { VIN: "58490394", make: "Toyota", model: "Camry", mileage: "200000" }
       ]);
     });
 };
